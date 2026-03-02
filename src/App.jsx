cd39d6d4d7ac3757c71b1859fdf08577fe740c63
emanOsamaMohamedKamal--hub <emanOsamaMohamedKanal@gmail.com>
2026-03-02 19:48:38 +0200

    remove App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
  import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./App.css"
-  import {HeroUIProvider,Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button, ToastProvider} from "@heroui/react";
+   import {HeroUIProvider,Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button, ToastProvider} from "@heroui/react";
 import MainLayout from "./layout/MainLayout";
 import Feed from "./pages/Feed";
 import Profile from "./pages/Profile";
